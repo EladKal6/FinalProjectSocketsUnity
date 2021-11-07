a28e681f034242417eacd69c245b86c9e3a4573d
EladKal6 <eladkal6@gmail.com>
2021-11-07 10:09:58 +0200

    importing the project into git hub
    the server is inside of unity and there is a player controller

---
+++ NewClient/Assets/NetworkConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using KaymakNetwork.Network;
+
+
+
+internal static class NetworkConfig
+{
+    internal static Client socket;
+
+    internal static void InitNetWork()
+    {
+        if (!ReferenceEquals(socket, null)) return;
+        socket = new Client(100);
+        NetworkReceive.PacketRouter();
+    }
+
+    internal static void ConnectToServer()
+    {
+        socket.Connect("localhost", 5555);
+    }
+
+    internal static void DisconnectFromServer()
+    {
+        socket.Dispose();
+    }
+}
+
---
+++ NewClient/Assets/NetworkSend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using KaymakNetwork;
+
+
+enum ClientPackets
+{
+    CPing = 1,
+}
+
+internal static class NetworkSend
+{
+    public static void SendPing()
+    {
+        ByteBuffer buffer = new ByteBuffer(4);
+        buffer.WriteInt32((int)ClientPackets.CPing);
+        buffer.WriteString("Hello I am the Client thank you for letting me connect to the server!");
+        NetworkConfig.socket.SendData(buffer.Data, buffer.Head);
+
+        buffer.Dispose();
+    }
+}
+
